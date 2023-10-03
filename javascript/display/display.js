401976396d3035a3f1f6ef8195836440831494ad
Grantas Joniškis <grantasjo@gmail.com>
2023-10-03 15:36:36 +0300

    Refactoring display elements.

--- javascript/display/display.js
+++ javascript/display/display.js
@@ -1,25 +1,34 @@
-export const displayElements = {
-  heroHeader: document.getElementById('heroHeader'), // GAME_START
-  mainHeader: document.getElementById('mainHeader'), // CHOICE, ANIMATION, RESULT
-  subHeader: document.getElementById('subHeader'), // CHOICE
-  splitHeader: document.getElementById('splitHeader'), // ANIMATION
+class Display {
+  static displayElements = {
+    heroHeader: document.getElementById('heroHeader'), // GAME_START
+    mainHeader: document.getElementById('mainHeader'), // CHOICE, ANIMATION, RESULT
+    subHeader: document.getElementById('subHeader'), // CHOICE
+    splitHeader: document.getElementById('splitHeader'), // ANIMATION
+    
+    gameMain: document.getElementById('gameMain'), //
+    startGame: document.getElementById('startGame'), // GAME_START
+    
+    subHeaderResult: document.getElementById('subHeaderResult'); // RESULT
+    roundCountContainerTop: document.getElementById('roundCountContainerTop'), // RESULT
   
-  gameMain: document.getElementById('gameMain'),
-  startGame: document.getElementById('startGame'), // GAME_START
+    circles: document.getElementsByClassName('circle'); // RESULT, END
   
-  iconContainer: document.getElementById('iconContainer'), // CHOICE
-  rockCard: document.getElementById('rockCard'), // CHOICE
-  paperCard: document.getElementById('paperCard'), // CHOICE
-  scissorsCard: document.getElementById('scissorsCard'), // CHOICE
+    iconContainer: document.getElementById('iconContainer'), // CHOICE
+    rockCard: document.getElementById('rockCard'), // CHOICE
+    paperCard: document.getElementById('paperCard'), // CHOICE
+    scissorsCard: document.getElementById('scissorsCard'), // CHOICE
+    youCard: document.getElementById('youCard'); // RESULT
+    aiCard: document.getElementById('aiCard'); // RESULT
   
-  roundCountContainer: document.getElementById('roundCountContainer'),
+    roundCountContainerBottom: document.getElementById('roundCountContainerBottom'), // END
+  }
 
-  // JavaScript generated HTML
-  // gameCardsContainer: document.getElementById('gameCardsContainer'),
-  // cardInGamePlayer: document.getElementById('cardInGamePlayer'),
-  // cardInGameAi: document.getElementById('cardInGameAi'),
+  static visibleElements(...elements) {
+    Object.keys(displayElements).forEach(element => {
+      if (elements.includes(element)) Display.displayElements[element].classList.remove('none');
+      else displayElements[element].classList.add('none');
+    });
+  }
 }
 
-export const displayElementsHTML = {
-  gameMainHTML: document.getElementById('gameMain').innerHTML,
-}
+export default Display;
--- javascript/game/game-manager.js
+++ javascript/game/game-manager.js
@@ -1,5 +1,5 @@
 import gameState from './game-state.js';
-import {displayElements, displayElementsHTML} from '../display/display.js';
+import Display from '../display/display.js';
 import Round from '../round/round.js';
 
 class GameManager {
@@ -21,25 +21,16 @@ class GameManager {
   updateDisplay() {
       switch (this.#gameState) {
         case gameState.START:
-          this.#visibleElements('heroHeader', 'gameMain', 'startGame');
-        break;
+          Display.visibleElements('heroHeader', 'gameMain', 'startGame');
+          break;
 
         case gameState.CHOICE:
-          this.#visibleElements('mainHeader', 'subHeader', 'gameMain', 'iconContainer', 'rockCard', 'paperCard', 'scissorsCard');
-        break;
+          Display.visibleElements('mainHeader', 'subHeader', 'gameMain', 'iconContainer', 'rockCard', 'paperCard', 'scissorsCard');
+          break;
 
         case gameState.ANIMATION:
-          this.#visibleElements('mainHeader', 'splitHeader', 'gameMain');
-
-          this.forAnimationCase();
-
-          displayElements.gameCardsContainer = document.getElementById('gameCardsContainer');
-          displayElements.cardInGamePlayer = document.getElementById('cardInGamePlayer');
-          displayElements.cardInGameAi = document.getElementById('cardInGameAi');
-
-          console.log(displayElements.gameMain.innerHTML);
-
-        break;
+          Display.visibleElements('mainHeader', 'splitHeader', 'gameMain');
+          break;
 
         case gameState.RESULT:
           const visibleCards = [];
@@ -57,25 +48,10 @@ class GameManager {
             break;
           }
 
-          this.#visibleElements('mainHeader', 'gameMain', 'roundCountContainer', 'gameCardsContainer', ...visibleCards);
-          console.log(displayElements.gameMain.innerHTML);
+          Display.visibleElements('mainHeader', 'gameMain', 'roundCountContainer', 'gameCardsContainer', ...visibleCards);
         break;
     }
   }
-
-  #visibleElements(...elements) {
-    Object.keys(displayElements).forEach(element => {
-      if (elements.includes(element)) {
-        displayElements[element].classList.remove('none');
-        if(this.#gameState === gameState.RESULT) console.log(element, displayElements[element]);
-        if(this.#gameState === gameState.RESULT) console.log(displayElements[element].classList.contains('none'));
-      }
-      else {
-        displayElements[element].classList.add('none');
-        if(this.#gameState === gameState.RESULT) console.log(element, displayElements[element]);
-        if(this.#gameState === gameState.RESULT) console.log(displayElements[element].classList.contains('none'));
-      }
-    });
   }
 
   pushRound(round) {
@@ -106,73 +82,6 @@ class GameManager {
 
   getGameState() {
     return this.#gameState;
-  }
-
-  forAnimationCase() {
-    // Create gameCardsContainer div
-    const gameCardsContainer = document.createElement('div');
-    gameCardsContainer.className = 'gameCardsContainer';
-    gameCardsContainer.id = 'gameCardsContainer';
-
-    // Create cardInGamePlayer div
-    const cardInGamePlayer = document.createElement('div');
-    cardInGamePlayer.className = 'cardInGame cardInGamePlayer';
-    cardInGamePlayer.id = 'cardInGamePlayer';
-
-    // Add appropriate content to cardInGamePlayer based on player choice
-    let playerIcon, playerText;
-    switch (this.#playerChoice) {
-      case 'rock':
-        playerIcon = '<i class="fa-regular fa-hand-back-fist fa-rotate-90 fa-2xl"></i>';
-        playerText = 'Rock';
-        break;
-      case 'paper':
-        playerIcon = '<i class="fa-regular fa-hand fa-rotate-90 fa-2xl"></i>';
-        playerText = 'Paper';
-        break;
-      case 'scissors':
-        playerIcon = '<i class="fa-regular fa-hand-scissors fa-flip-horizontal fa-2xl"></i>';
-        playerText = 'Scissors';
-        break;
-    }
-    cardInGamePlayer.innerHTML = `${playerIcon}<h4>${playerText}</h4>`;
-
-    // Create cardInGameAi div
-    const cardInGameAi = document.createElement('div');
-    cardInGameAi.className = 'cardInGame cardInGameAi';
-    cardInGameAi.id = 'cardInGameAi';
-
-    // Add appropriate content to cardInGameAi based on bot choice
-    let botChoice = this.#rounds[this.#currentRound-1].getBotChoice();
-    let botIcon, botText;
-    switch (botChoice) {
-      case 'rock':
-        botIcon = '<i class="fa-regular fa-hand-back-fist fa-rotate-90 fa-2xl"></i>';
-        botText = 'Rock';
-        break;
-      case 'paper':
-        botIcon = '<i class="fa-regular fa-hand fa-rotate-90 fa-2xl"></i>';
-        botText = 'Paper';
-        break;
-      case 'scissors':
-        botIcon = '<i class="fa-regular fa-hand-scissors fa-flip-horizontal fa-2xl"></i>';
-        botText = 'Scissors';
-        break;
-    }
-    cardInGameAi.innerHTML = `${botIcon}<h4>${botText}</h4>`;
-
-    // Append the child divs to gameCardsContainer
-    gameCardsContainer.appendChild(cardInGamePlayer);
-    gameCardsContainer.appendChild(cardInGameAi);
-
-    // Append gameCardsContainer to gameMain
-    displayElements.gameMain.appendChild(gameCardsContainer);
-
-    // Update displayElements object
-    displayElements.gameCardsContainer = gameCardsContainer;
-    displayElements.cardInGamePlayer = cardInGamePlayer;
-    displayElements.cardInGameAi = cardInGameAi;
-
   }
 }
 
